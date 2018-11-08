5ec9eb20ebd4c22116d75488a94c3ae14047233c
Arnold Herod <arnold.herod@gmail.com>
2018-11-08 19:41:44 +0700

    Initial submission

--- beli.js
+++ beli.js
@@ -2,14 +2,56 @@ function beli(uang, obj, cb){
   console.log(`Saya pergi membeli ${obj.item}`)
   setTimeout(function(){
     let kembalian = uang - obj.harga
-    if (kembalian > 0) {
+    if (kembalian <= 0) {
+      console.log(`uang gk cukup nih buat beli ${obj.item} kembaliannya cuma ${uang}`)
+    }
+    else{
       console.log(`Saya sudah membeli ${obj.item} uang kembaliannya ${kembalian}`);
       cb(kembalian)
-    }else{
-      console.log(`uang gk cukup nih buat beli ${obj.item} kembaliannya cuma ${kembalian}`);
-      cb(0)
     }
   }, obj.waktu);
 }
+
+let cash = 100
+let itemList = [
+  {
+    item: "permen",
+    harga: 50,
+    waktu: 1000
+  },
+  {
+    item: "roti",
+    harga: 150,
+    waktu: 1000
+  },
+  {
+    item: "HP",
+    harga: 500,
+    waktu: 1000
+  },
+  {
+    item: "sticker",
+    harga: 10,
+    waktu: 1000
+  },
+  {
+    item: "drinks",
+    harga: 90,
+    waktu: 1000
+  }
+]
+
+beli(cash,itemList[0], function(kembalian){
+  beli(kembalian, itemList[1], function(kembalian2){
+    beli(kembalian2, itemList[2], function(kembalian3){
+      beli(kembalian3, itemList[3], function(kembalian4){
+        beli(kembalian4,itemList[4], function(kembalian5){
+          console.log(kembalian5)
+        })
+      })
+    })
+  })
+})
+// console.log(kembalian)
 
 module.exports = beli;
