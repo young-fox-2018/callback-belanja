dcc4d411b3325265090892d8506211a30c6c5c74
imanuel vicky <imanuvi@imanuels-MBP.localdomain>
2018-11-08 19:15:32 +0700

    maybe done

--- beli.js
+++ beli.js
@@ -1,15 +1,24 @@
 function beli(uang, obj, cb){
-  console.log(`Saya pergi membeli ${obj.item}`)
+  if (uang < obj.harga) {
+      console.log("Duit g cukup broo")
+  } 
+  
   setTimeout(function(){
-    let kembalian = uang - obj.harga
-    if (kembalian > 0) {
+    if (uang-obj.harga >= 0) {
+      let kembalian = uang - obj.harga
+      console.log(`Saya pergi membeli ${obj.item}`)
       console.log(`Saya sudah membeli ${obj.item} uang kembaliannya ${kembalian}`);
       cb(kembalian)
     }else{
-      console.log(`uang gk cukup nih buat beli ${obj.item} kembaliannya cuma ${kembalian}`);
-      cb(0)
+      if (uang > 0) {
+          cb(uang)
+      } else {
+          cb(0) // buat apa code ini??
+         // console.log("masuk")
+          //console.log(`uang gk cukup nih buat beli ${obj.item} kembaliannya cuma ${kembalian}`);
+      }
     }
-  }, obj.waktu);
+}, obj.waktu);
 }
 
 module.exports = beli;
