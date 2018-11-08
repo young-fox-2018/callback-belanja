88861e8eb1d9a30593623858633b0bc98b45ac07
patriagani <patria.gani@gmail.com>
2018-11-08 14:59:25 +0700

    callback belanja done

---
+++ index.js
@@ -0,0 +1,43 @@
+const beli = require('./beli.js');
+
+var permen = {
+  item: "permen",
+  harga: 1000,
+  waktu: 1000
+}
+
+var bir = {
+  item: "bir",
+  harga: 75000,
+  waktu: 1000
+}
+
+var mekdi = {
+  item: "mekdi",
+  harga: 40000,
+  waktu: 1000
+}
+
+var beras = {
+  item: "beras",
+  harga: 60000,
+  waktu: 1000
+}
+
+var cerutu = {
+  item: "cerutu",
+  harga: 150000,
+  waktu: 1000
+}
+
+beli(500000, permen, function(kembalian){
+  beli(kembalian, bir, function(kembalian){
+    beli(kembalian, mekdi, function(kembalian){
+      beli(kembalian, beras, function(kembalian){
+        beli(kembalian, cerutu, function(){
+          console.log("Selesai belanja, saya mau pesta !!!");
+        })
+      })
+    })
+  })
+})
